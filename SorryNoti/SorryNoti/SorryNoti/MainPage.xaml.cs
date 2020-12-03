5ab76db6bd25c3798e4835c2acef6ef08c047799
arleng2017 <arleng2017@gmail.com>
2020-12-03 10:04:29 +0700

    add
    Signed-off-by: arleng2017 <arleng2017@gmail.com>

---
+++ SorryNoti/SorryNoti/SorryNoti/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SorryNoti
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SendNotification(object sender, EventArgs e)
+        {
+            await Task.Delay(5000);
+            DependencyService.Get<INotification>().CreateNotification("SPTutorials", message.Text);
+        }
+    }
+}
