20e246ae6cc13dc61f40db8c7570d90b3eee3a07
arleng2017 <arleng2017@gmail.com>
2020-12-03 10:35:32 +0700

    add Noti
    Signed-off-by: arleng2017 <arleng2017@gmail.com>

---
+++ SorryNoti/SorryNoti/SorryNoti.iOS/NotificationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using SorryNoti.iOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(NotificationHelper))]
+
+namespace SorryNoti.iOS
+{
+    class NotificationHelper : INotification
+    {
+        public async void CreateNotification(string title, string message)
+        {
+            new NotificationDelegate().RegisterNotification(title, message);
+        }
+    }
+}
