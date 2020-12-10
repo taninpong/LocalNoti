d1b71e68918d67228fd3cb85fda06b61a8e9e45c
taninpong <taninpong.ch.56@ubu.ac.th>
2020-12-10 11:31:39 +0700

    E4

--- SorryNoti/SorryNoti/SorryNoti.iOS/NotificationDelegate.cs
+++ SorryNoti/SorryNoti/SorryNoti.iOS/NotificationDelegate.cs
@@ -52,7 +52,7 @@ namespace SorryNoti.iOS
                 AlertTitle = title,
                 AlertBody = body,
                 ApplicationIconBadgeNumber = 1,
-                SoundName = "police.aiff"
+                SoundName = "police.wav"
             };
             UIApplication.SharedApplication.ScheduleLocalNotification(noti);
             //UNUserNotificationCenter center = UNUserNotificationCenter.Current;
